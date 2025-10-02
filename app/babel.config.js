1238a7f8cb151fbc0244ae6768a0fde1027b4f84
Shawn Flahave <shawnflahave@gmail.com>
2025-10-02 14:57:46 -0500

    tried using react compiler but not sure if that is causing problems.

--- app/babel.config.js
+++ app/babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
-      'react-native-worklets/plugin',
+    // 'babel-plugin-react-compiler', // Disabled for now
+    'react-native-worklets/plugin',
     ],
 };
